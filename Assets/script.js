e7cfeb07df00f8187aabfd97266fdc652b8743c9
Arcene LB <55070235+lbernadel@users.noreply.github.com>
2019-11-14 17:38:49 -0500

    added all info for easy version correctly and changed font for legibility

--- Assets/script.js
+++ Assets/script.js
@@ -15,13 +15,21 @@ function displayWeather() {
         now = moment().format('MM/DD/YY'),
         icon = $("<img src='http://openweathermap.org/img/wn/"+ response.weather[0].icon +".png' > ");
         
+        // creating new element, add Bootstrap class attr, append info properly from API response
         newTitle = $('<h3>').attr('class','card-title').append(response.name + " (" + now + ") "  ).append(icon);
-        newTemp = $('<p>').attr('class', 'card-text').append("Current Temp: " + response.main.temp + " °F")
-         
-        $(newCityCard).append(newTitle, newTemp);
+        newTemp = $('<p>').attr('class', 'card-text').append("Current Temp: " + response.main.temp + " °F");
+        newHumidity = $('<p>').attr('class', 'card-text').append("Humidity: " + response.main.humidity + "%");
+        newWind = $('<p>').attr('class', 'card-text').append("Wind Speed: " + response.wind.speed + " mph")
+        
+        // append the information to the card
+        $(newCityCard).append(newTitle, newTemp, newHumidity, newWind);
+
+        // append the card to the page for display
         $('.weather-data').append(newCityCard);
         
       });
+      // clear input field once card gets appended
+      $("#cityInput").val("");
 };
 
 // click listener for search button
