67e4f5def99c7093f03b536bc091e1728ecdad72
Arcene LB <55070235+lbernadel@users.noreply.github.com>
2019-11-14 21:41:26 -0500

    added ajax call for UV Index

--- Assets/script.js
+++ Assets/script.js
@@ -13,13 +13,24 @@ function displayWeather() {
         newCityBody = $('<div class="card-body py-2">'),
         newCityCard = $(newCard).append(newCityBody),
         now = moment().format('MM/DD/YY'),
-        icon = $("<img src='http://openweathermap.org/img/wn/"+ response.weather[0].icon +".png' > ");
+        icon = $("<img src='http://openweathermap.org/img/wn/"+ response.weather[0].icon +".png' > "),
+        lat = response.coord.lat,
+        lon = response.coord.lon;
         
+        // getting and appending UV Index using coords from initial ajax call
+        $.ajax({
+          url: uvURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + lat + "&lon=" + lon,
+          method: "GET"
+        }).then(function(data){
+          newUV = $('<p>').attr('class','card-text').append("UV Index: " + data.value);
+          $(newCityCard).append(newUV);
+        });
+       
         // creating new element, add Bootstrap class attr, append info properly from API response
         newTitle = $('<h3>').attr('class','card-title').append(response.name + " (" + now + ") "  ).append(icon);
         newTemp = $('<p>').attr('class', 'card-text').append("Current Temp: " + response.main.temp + " °F");
         newHumidity = $('<p>').attr('class', 'card-text').append("Humidity: " + response.main.humidity + "%");
-        newWind = $('<p>').attr('class', 'card-text').append("Wind Speed: " + response.wind.speed + " mph")
+        newWind = $('<p>').attr('class', 'card-text').append("Wind Speed: " + response.wind.speed + " mph");
         
         // append the information to the card
         $(newCityCard).append(newTitle, newTemp, newHumidity, newWind);
